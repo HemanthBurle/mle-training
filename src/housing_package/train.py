f53811141560ec32617520c7a0692f326f835a1d
BURLE HEMANTH <101197659+HemanthBurle@users.noreply.github.com>
2023-07-10 16:57:14 +0530

    Update train.py

--- src/housing_package/train.py
+++ src/housing_package/train.py
@@ -86,100 +86,100 @@ if __name__ == "__main__":
     with open(PICKLE_PATH, "rb") as file:
         y_test = pickle.load(file)
 
-    def train_models():
-        """This function train all the models like linear regression,random forest etc.
+def train_models():
+    """This function train all the models like linear regression,random forest etc.
 
-            Parameters
-            ----------
+        Parameters
+        ----------
 
 
-            Returns
-            -------
-        Creates pickle files for all the models and store those files in artifacts
+        Returns
+        -------
+    Creates pickle files for all the models and store those files in artifacts
 
-        """
-        lin_reg = LinearRegression()
-        lin_reg.fit(housing_prepared, housing_labels)
+    """
+    lin_reg = LinearRegression()
+    lin_reg.fit(housing_prepared, housing_labels)
 
-        housing_predictions = lin_reg.predict(housing_prepared)
-        lin_mse = mean_squared_error(housing_labels, housing_predictions)
-        lin_rmse = np.sqrt(lin_mse)
-        lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+    housing_predictions = lin_reg.predict(housing_prepared)
+    lin_mse = mean_squared_error(housing_labels, housing_predictions)
+    lin_rmse = np.sqrt(lin_mse)
+    lin_mae = mean_absolute_error(housing_labels, housing_predictions)
 
-        output_file = os.path.join(ARTIFACTS_PATH, "lin_reg_op.pkl")
-        with open(output_file, "wb") as f:
-            pickle.dump([lin_rmse, lin_mae], f)
+    output_file = os.path.join(ARTIFACTS_PATH, "lin_reg_op.pkl")
+    with open(output_file, "wb") as f:
+        pickle.dump([lin_rmse, lin_mae], f)
 
-        tree_reg = DecisionTreeRegressor(random_state=42)
-        tree_reg.fit(housing_prepared, housing_labels)
+    tree_reg = DecisionTreeRegressor(random_state=42)
+    tree_reg.fit(housing_prepared, housing_labels)
 
-        housing_predictions = tree_reg.predict(housing_prepared)
-        tree_mse = mean_squared_error(housing_labels, housing_predictions)
-        tree_rmse = np.sqrt(tree_mse)
+    housing_predictions = tree_reg.predict(housing_prepared)
+    tree_mse = mean_squared_error(housing_labels, housing_predictions)
+    tree_rmse = np.sqrt(tree_mse)
 
-        output_file = os.path.join(ARTIFACTS_PATH, "tree_reg_op.pkl")
-        with open(output_file, "wb") as f:
-            pickle.dump(tree_rmse, f)
+    output_file = os.path.join(ARTIFACTS_PATH, "tree_reg_op.pkl")
+    with open(output_file, "wb") as f:
+        pickle.dump(tree_rmse, f)
 
-        param_distribs = {
-            "n_estimators": randint(low=1, high=200),
-            "max_features": randint(low=1, high=8),
-        }
+    param_distribs = {
+        "n_estimators": randint(low=1, high=200),
+        "max_features": randint(low=1, high=8),
+    }
 
-        forest_reg = RandomForestRegressor(random_state=42)
-        rnd_search = RandomizedSearchCV(
-            forest_reg,
-            param_distributions=param_distribs,
-            n_iter=10,
-            cv=5,
-            scoring="neg_mean_squared_error",
-            random_state=42,
-        )
-        rnd_search.fit(housing_prepared, housing_labels)
-        cvres = rnd_search.cv_results_
+    forest_reg = RandomForestRegressor(random_state=42)
+    rnd_search = RandomizedSearchCV(
+        forest_reg,
+        param_distributions=param_distribs,
+        n_iter=10,
+        cv=5,
+        scoring="neg_mean_squared_error",
+        random_state=42,
+    )
+    rnd_search.fit(housing_prepared, housing_labels)
+    cvres = rnd_search.cv_results_
 
-        output_file = os.path.join(ARTIFACTS_PATH, "cvres.pkl")
-        with open(output_file, "wb") as f:
-            pickle.dump(cvres, f)
+    output_file = os.path.join(ARTIFACTS_PATH, "cvres.pkl")
+    with open(output_file, "wb") as f:
+        pickle.dump(cvres, f)
 
-        param_grid = [
-            # try 12 (3×4) combinations of hyperparameters
-            {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
-            # then try 6 (2×3) combinations with bootstrap set as False
-            {"bootstrap": [False], "n_estimators": [3, 10], "max_features": [2, 3, 4]},
-        ]
+    param_grid = [
+        # try 12 (3×4) combinations of hyperparameters
+        {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
+        # then try 6 (2×3) combinations with bootstrap set as False
+        {"bootstrap": [False], "n_estimators": [3, 10], "max_features": [2, 3, 4]},
+    ]
 
-        forest_reg = RandomForestRegressor(random_state=42)
-        # train across 5 folds, that's a total of (12+6)*5=90 rounds of training
-        grid_search = GridSearchCV(
-            forest_reg,
-            param_grid,
-            cv=5,
-            scoring="neg_mean_squared_error",
-            return_train_score=True,
-        )
-        grid_search.fit(housing_prepared, housing_labels)
+    forest_reg = RandomForestRegressor(random_state=42)
+    # train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+    grid_search = GridSearchCV(
+        forest_reg,
+        param_grid,
+        cv=5,
+        scoring="neg_mean_squared_error",
+        return_train_score=True,
+    )
+    grid_search.fit(housing_prepared, housing_labels)
 
-        grid_search.best_params_
-        cvres = grid_search.cv_results_
+    grid_search.best_params_
+    cvres = grid_search.cv_results_
 
-        output_file = os.path.join(ARTIFACTS_PATH, "new_cvres.pkl")
-        with open(output_file, "wb") as f:
-            pickle.dump(cvres, f)
+    output_file = os.path.join(ARTIFACTS_PATH, "new_cvres.pkl")
+    with open(output_file, "wb") as f:
+        pickle.dump(cvres, f)
 
-        feature_importances = grid_search.best_estimator_.feature_importances_
-        sorted(zip(feature_importances, housing_prepared.columns), reverse=True)
+    feature_importances = grid_search.best_estimator_.feature_importances_
+    sorted(zip(feature_importances, housing_prepared.columns), reverse=True)
 
-        final_model = grid_search.best_estimator_
+    final_model = grid_search.best_estimator_
 
-        final_predictions = final_model.predict(X_test_prepared)
-        final_mse = mean_squared_error(y_test, final_predictions)
-        final_rmse = np.sqrt(final_mse)
+    final_predictions = final_model.predict(X_test_prepared)
+    final_mse = mean_squared_error(y_test, final_predictions)
+    final_rmse = np.sqrt(final_mse)
 
-        output_file = os.path.join(ARTIFACTS_PATH, "final_rmse.pkl")
-        with open(output_file, "wb") as f:
-            pickle.dump(final_rmse, f)
+    output_file = os.path.join(ARTIFACTS_PATH, "final_rmse.pkl")
+    with open(output_file, "wb") as f:
+        pickle.dump(final_rmse, f)
 
-        logging.info("The train script ran successfully")
+    logging.info("The train script ran successfully")
 
-    train_models()
+train_models()
